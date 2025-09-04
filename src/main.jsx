e10fc5c553b7153dade7919e9f01edfda6b4d623
Ramesh Budha <rawmess977@gmail.com>
2025-09-04 19:45:46 +0530

    register and login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { lazy, } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-const App = lazy(()=>import('./App'))
-import { BrowserRouter } from 'react-router-dom'
+import { lazy, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+const App = lazy(() => import("./App"));
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Suspense>
+      <App />
+    </Suspense>
+  </BrowserRouter>
+);
--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,6 +1,89 @@
+import { Link } from "react-router-dom";
+import {FaGoogle, FaFacebook} from 'react-icons/fa'
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div className="min-w-screen min-h-screen bg-[$cdcae9] flex justify-center items-center ">
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className="bg-[#6f68d1] p-4 rounded-md">
+          <h2 className="text-xl mb-3 font-bold">Welcome to E-Commerce</h2>
+          <p className="text-sm mb-3 font-medium">
+            Please Login your account
+          </p>
+          <form action="">
+            
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-medium "
+                type="email"
+                name="email"
+                placeholder="Email"
+                id="email"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Password</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-medium "
+                type="password"
+                name="password"
+                placeholder="Email"
+                id="password"
+                required
+              />
+            </div>
+            {/* <div className="flex items-center w-full gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="checkbox"
+                id="checkbox"
+                className="text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300  focus:ring-blue-500"
+              />
+              <label htmlFor="checkbox">
+                {" "}
+                I agree to privacy policy & terms
+              </label>
+            </div> */}
+
+            <button className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
+              Login
+            </button>
+
+            <div className="flex items-center mb-3 gap-3 justify-center">
+              <p>
+                Don't have an account? {" "}
+                <Link className="font-bold" to="/register">
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+
+            <div className="w-full flex justify-center items-center mb-3">
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[10%] flex justify-center items-center">
+                <span className="pb-1">Or</span>
+              </div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+            </div>
+            <div className="flex justify-center items-center gap-3">
+              <div className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-md hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span>
+                  <FaGoogle />
+                </span>
+                
+              </div>
+              <div className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-md hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span>
+                  <FaFacebook />
+                </span>
+                
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
