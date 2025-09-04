0e4cb66def65d76980f2effaa87b4a3c38cd30d8
Ramesh Budha <rawmess977@gmail.com>
2025-09-04 18:01:11 +0530

    login register routes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { lazy, } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+const App = lazy(()=>import('./App'))
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
---
+++ src/router/routes/PublicRoutes.jsx
@@ -0,0 +1,19 @@
+import { lazy    } from "react"; //is used for code splitting in React — meaning it lets you load components only when they are needed, instead of bundling everything upfront.
+
+const Login = lazy(()=>import("../../views/auth/Login"))
+const Register = lazy(()=>import("../../views/auth/Register"))
+
+const publicRoutes = [
+    {
+        path: '/login',
+        element: <Login />
+    }, 
+    {
+        path: '/register',
+        element: <Register />
+    }
+   
+]
+export default publicRoutes;
+
+
