e68b163bddefdf5102a66558d0588b37f3adcbc7
Ramesh Budha <rawmess977@gmail.com>
2025-09-06 09:48:59 +0530

    react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 const App = lazy(() => import("./App"));
 import { BrowserRouter } from "react-router-dom";
-
+import store from "./store/store";
+import { Provider } from "react-redux";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Suspense>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense>
+        <App />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 );
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-import { FaGoogle, FaFacebook } from "react-icons/fa";
+
 import { useState } from "react";
 
 const AdminLogin = () => {
