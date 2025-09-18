bdbdd8d3fe6bbedbf9b401518d285d84b4f17d89
Ramesh Budha <rawmess977@gmail.com>
2025-09-18 15:31:34 +0530

    auth reducer

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,35 +1,52 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import api from './../../api/api';
+import api from "./../../api/api";
 
 export const admin_login = createAsyncThunk(
-
-    'auth/admin_login',
-    async(info) => {
-        console.log(info)
-        try{
-            // const {data} = await api.post('/admin-login', info,{ withCredentials: true})
-            // console.log(data)
-        }
-        catch (error){ 
-            console.log(error)
-        }
+  "auth/admin_login",
+  async (info, {rejectWithValue, fulfillWithValue}) => {
+    // console.log(info)
+    try {
+      const { data } = await api.post("/admin-login", info, {
+        withCredentials: true,
+      });
+      return fulfillWithValue(data)
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Login Failed")
     }
-)
+  }
+);
 
-export const authReducer = createSlice({
-    name: 'auth',
-    initialState:{
-        successMessage: '',
-        errorMessage: '',
-        loader: false,
-        userInfo: ''
-    },
-    reducers: {
-
-    },
-    extraReducers: ()=> {
-
+const authReducer = createSlice({
+  name: "auth",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    userInfo: "",
+  },
+  reducers: {
+    messageClear : (state) => {
+      state.errorMessage = "";
+      state.successMessage = "";
     }
-})
+  },
+  extraReducers: (builder) => {
+		builder
+			.addCase(admin_login.pending, (state)=>{
+				state.loader = true;
 
+			})
+			.addCase(admin_login.rejected, (state, {payload})=>{
+				state.loader = false;
+        state.errorMessage = payload.error;
+
+			})
+			.addCase(admin_login.fulfilled, (state, {payload})=>{
+				state.loader = false;
+        state.successMessage = payload.message;
+
+			})
+	},
+});
+export const {messageClear} =authReducer.actions;
 export default authReducer.reducer;
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,10 +1,16 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { admin_login, messageClear } from "../../store/Reducers/authReducer";
 
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { admin_login } from "../../store/Reducers/authReducer";
-
+import { PropagateLoader } from "react-spinners";
+import { toast } from "react-hot-toast";
+import {useNavigate} from 'react-router-dom'
 const AdminLogin = () => {
-  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const { loader, errorMessage, successMessage } = useSelector(
+    (state) => state.auth
+  );
   const [state, setState] = useState({
     email: "",
     password: "",
@@ -19,18 +25,34 @@ const AdminLogin = () => {
 
   const submit = (e) => {
     e.preventDefault();
-    dispatch(admin_login(state))
-    
+    dispatch(admin_login(state));
   };
+
+  useEffect(() => {
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+  }, [errorMessage]);
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+      navigate('/')
+    }
+  }, [successMessage]);
 
   return (
     <div className="min-w-screen min-h-screen bg-[$cdcae9] flex justify-center items-center ">
       <div className="w-[350px] text-[#ffffff] p-2">
         <div className="bg-[#6f68d1] p-4 rounded-md">
-          
           <div className="h-[70px] flex justify-center items-center">
             <div className="w-[180px] h-[50px]">
-              <img src="../../../public/images/logo.png" alt="logo" className="w-full h-full" />
+              <img
+                src="../../../public/images/logo.png"
+                alt="logo"
+                className="w-full h-full"
+              />
             </div>
           </div>
           <form action="" onSubmit={submit}>
@@ -61,11 +83,25 @@ const AdminLogin = () => {
               />
             </div>
 
-            <button className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mt-4 ">
-              Login
+            <button
+              disabled={loader ? true : false}
+              className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mt-4 "
+            >
+              {loader ? (
+                <PropagateLoader
+                  color="#fff"
+                  cssOverride={{
+                    display: "flex",
+                    margin: "0 auto",
+                    height: "24px",
+                    justifyContent: "center",
+                    alignContent: "center",
+                  }}
+                />
+              ) : (
+                "Login"
+              )}
             </button>
-
-            
           </form>
         </div>
       </div>
